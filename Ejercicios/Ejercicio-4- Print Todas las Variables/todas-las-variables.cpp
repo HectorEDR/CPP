7bd2f9d5ddba478d8355335488199b405a4b069b
Hector Rapalo <hector.rapalo@unah.hn>
2020-07-07 10:56:39 -0600

    Agregando ejercicio 4 - Todas variables

---
+++ Ejercicios/Ejercicio-4- Print Todas las Variables/todas-las-variables.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bool valorboolean;
+    valorboolean = false;
+
+    int valorentero = 15;
+
+    double valordouble = 19.89;
+
+    string valorstring = "Hola Mundo";
+
+    char valorchar = 'A';
+
+    cout << "Valor Boolean es: " << valorboolean << endl;
+    cout << "Valor Int es: " << valorentero << endl;
+    cout << "Valor Double es: " << valordouble << endl;
+    cout << "Valor String es: " << valorstring << endl;
+    cout << "Valor Char es: " << valorchar << endl;
+
+    return 0;
+}
