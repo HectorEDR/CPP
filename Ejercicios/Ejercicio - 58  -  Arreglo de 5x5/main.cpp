14ec0897356962e24ba475930106a0c44a68f00b
Hector Rapalo <hector.rapalo@unah.hn>
2020-08-14 20:34:31 -0600

    Mostrando arreglo

--- Ejercicios/Ejercicio - 58  -  Arreglo de 5x5/main.cpp
+++ Ejercicios/Ejercicio - 58  -  Arreglo de 5x5/main.cpp
@@ -6,23 +6,27 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int suma;
+    int suma = 0;
 
     int arreglo[5][5];
 
     srand(time(NULL));
+    
+    cout << endl;
 
     for (int filas = 0; filas < 5; filas++)
     {
         for (int columnas = 0; columnas < 5; columnas++)
         {
-            arreglo[filas][columnas] = rand() %  5 +1;
+            arreglo[filas][columnas] = rand() %  9 + 1;
+            cout << "[" << arreglo[filas][columnas] << "]" << " ";
             
             suma = suma + arreglo[filas][columnas];
         }  
-    } cout << "El total suma: " << suma;
-    
-    
+            cout << endl;
+    } 
+    cout << endl;
+    cout << "El total suma: " << suma << endl;
 
     return 0;
 }
