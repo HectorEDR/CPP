9687d69061a4e3af0b9d149ce27aadf2b8fe42bd
HectorEDR <67705568+HectorEDR@users.noreply.github.com>
2020-08-23 14:14:24 -0600

    Actualizando buscar citas

--- PROYECTO/buscarcitas.cpp
+++ PROYECTO/buscarcitas.cpp
@@ -7,7 +7,7 @@ void mostrarBusquedaCita()
 {
   system("cls");
   string c_pax = "";
-  string citaPaciente = "";
+  
   char continuar = ' ';
   cout << endl;
   cout << " REGISTRO DE CITAS"<< endl;
@@ -19,7 +19,7 @@ void mostrarBusquedaCita()
   {
      cout << "Ingrese el codigo del paciente: ";
      cin >> c_pax;
-     citaPaciente = buscarPaciente(c_pax);
+     buscarPaciente(c_pax);
 
       cout << endl;
       cout << endl;
